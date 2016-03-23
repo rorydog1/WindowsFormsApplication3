61fbc954932f63fe61140e97ab9c45e451c72c89
rorydog1 <rorydog1@gmail.com>
2016-03-23 01:52:37 +0000

    windows startup bat
    winbat

---
+++ win32.bat
@@ -0,0 +1,4 @@
+
+set PATH=C:\Users\rory\Desktop\WindowsFormsApplication3\WindowsFormsApplication3\obj\EPICS.NET\
+
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" WindowsFormsApplication3.sln
